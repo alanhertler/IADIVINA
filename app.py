82551b6c65da2750742f56e740001a03b63c6ced
alanhertler <alanhertler@gmail.com>
2026-03-25 13:29:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -109,9 +109,11 @@ def clasificar_riesgo(texto: str) -> str:
 
     patrones_abuso = [
         r"\bmi papa me toco\b",
-        r"\bme toco entre las piernas\b",
+        r"\bme tocaron entre las piernas\b"
+        r"\bme tocaron\b.+\bpiernas\b"
         r"\bme manosearon\b",
         r"\bme tocaron los senos\b",
+        r"\bme toco un seno\b",
         r"\bme tocaron la cola\b",
         r"\bme tocaron la vagina\b",
         r"\babusaron de mi\b",
